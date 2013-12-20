2c8d61055c0e09dc9afd552294a0f4dfb7bb412d
Ronnie Nigash <nigash.ronnie@gmail.com>
2013-12-20 13:18:07 -0800

    First Commit
    First rendition of the Hello World program. Prints list of names and
    some sample greetings to the console. See what you can add to it! :D

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+// @author Ronnie Nigash
+// Our first push/pull test file
+// Makes a list to add names to, then prints the list to the console with different greetings for each name
+import java.util.ArrayList;
+import java.util.*;
+import java.util.Random;
+public class HelloWorld {
+	
+	public static void main(String[] Args){
+		ArrayList<String> nameList = new ArrayList<String>();
+		Random generator = new Random();
+		
+		nameList.add("Ronnie Nigash");
+		
+		loopDaLoop(nameList, generator);
+	}
+		
+	private static void loopDaLoop (ArrayList<String> list, Random generator){
+		for(String name : list){
+			
+			List<String> greetList = Arrays.asList("hi!", "how are you?", "good morning.", "what's up?");
+			int randNum = generator.nextInt(greetList.size());
+			
+			System.out.printf("%s says,\"%s\"\n", name, greetList.get(randNum));
+		}
+	}
+}
