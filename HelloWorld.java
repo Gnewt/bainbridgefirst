a90d2ed6bddd470e92685e82b9eb07a1de9ac8ec
binnur <binnur.gul@gmail.com>
2013-12-20 16:20:33 -0800

    Added mentor 'binnur' to the list+ a new message
    Message 'Merhabalar!!' (hello) added

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,14 +11,15 @@ public class HelloWorld {
 		Random generator = new Random();
 		
 		nameList.add("Ronnie Nigash");
+                nameList.add("binnur");
 		
 		loopDaLoop(nameList, generator);
 	}
-		
+
 	private static void loopDaLoop (ArrayList<String> list, Random generator){
 		for(String name : list){
 			
-			List<String> greetList = Arrays.asList("hi!", "how are you?", "good morning.", "what's up?");
+			List<String> greetList = Arrays.asList("hi!", "how are you?", "good morning.", "what's up?", "Merhabalar!!");
 			int randNum = generator.nextInt(greetList.size());
 			
 			System.out.printf("%s says,\"%s\"\n", name, greetList.get(randNum));
