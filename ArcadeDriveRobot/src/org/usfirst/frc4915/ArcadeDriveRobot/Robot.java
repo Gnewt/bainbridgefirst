16a931b988161a28e1e7e596fbbf3d4646d9aeb1
TarkanAl-Kazily <tarkucar@mac.com>
2014-01-26 16:53:55 -0800

    Trying to fix git.

--- ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/Robot.java
+++ ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/Robot.java
@@ -10,7 +10,6 @@
 package org.usfirst.frc4915.ArcadeDriveRobot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -94,7 +93,13 @@ public class Robot extends IterativeRobot {
     public void teleopPeriodic() {
         Scheduler.getInstance().run();
         SmartDashboard.putBoolean("Pressure Switch", RobotMap.airCompressorCompressor.getPressureSwitchValue());
-        SmartDashboard.putNumber("Throttle", oi.getJoystickDrive().getThrottle());
+        SmartDashboard.putNumber("Throttle", oi.getJoystickDrive().getAxis(Joystick.AxisType.kThrottle));  
+        SmartDashboard.putNumber("NumAxis", oi.getJoystickDrive().getAxis(Joystick.AxisType.kNumAxis));
+        SmartDashboard.putNumber("X", oi.getJoystickDrive().getAxis(Joystick.AxisType.kX));
+        SmartDashboard.putNumber("Z", oi.getJoystickDrive().getAxis(Joystick.AxisType.kZ));
+        SmartDashboard.putNumber("Y", oi.getJoystickDrive().getAxis(Joystick.AxisType.kY));
+        SmartDashboard.putNumber("Twist", oi.getJoystickDrive().getAxis(Joystick.AxisType.kTwist));
+        
     }
     /**
      * This function called periodically during test mode
