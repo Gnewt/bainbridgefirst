c3511c9eda3221931af441bbaaaff6ed1ed4907a
DavidFaust <david@faust.us>
2014-01-29 16:54:55 -0800

    Added WindLauncher funcitonality

--- ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/subsystems/Launcher.java
+++ ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/subsystems/Launcher.java
@@ -41,7 +41,7 @@ public class Launcher extends Subsystem {
     public void extendPneumatics() {
         gearboxPneumatics.set(true);
     }
-    
+  
     /**
      * stop
      * 
@@ -58,5 +58,15 @@ public class Launcher extends Subsystem {
      */
     public boolean getStatePneumatics() {
         return gearboxPneumatics.get();
+    }
+      
+    public void startWindingMotor(double speed){
+        windingMotor.set(speed);
+    }
+    public void stopWindingMotor() {
+        windingMotor.set(0.0);
+    }
+    public double getWindingSpeed() {
+        return windingMotor.get();
     }
 }
