2317f1a51ff2cca9e3c83ca7daa312d3ff08c7df
TarkanAl-Kazily <tarkucar@mac.com>
2014-01-31 15:53:19 -0800

    Uniformity by making all objects referred from Robot.java or the top level class itself.

--- ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/commands/ArcadeDrive.java
+++ ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/commands/ArcadeDrive.java
@@ -35,7 +35,7 @@ public class  ArcadeDrive extends Command {
         
         Joystick joystickDrive = Robot.oi.getJoystickDrive();
         double joystickThrottle = .5 * (-1 * joystickDrive.getAxis(Joystick.AxisType.kThrottle) + 1.0); // Goes from 0 to 1, flipping the throttle on hardware to make sense
-        RobotMap.driveTrainRobotDrive.setMaxOutput(joystickThrottle); // Advanced Joystick goes from 0 to 1
+        Robot.driveTrain.setMaxOutput(joystickThrottle); // Advanced Joystick goes from 0 to 1
         // RobotMap.driveTrainRobotDrive.setMaxOutput(joystickDrive.getAxis(Joystick.AxisType.kZ)); // Attack Joystick
     }
     // Make this return true when this Command no longer needs to run execute()
