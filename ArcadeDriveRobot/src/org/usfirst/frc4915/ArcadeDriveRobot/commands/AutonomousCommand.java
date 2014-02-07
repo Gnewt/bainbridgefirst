150596baece867808445e8b2a19ab3f043ed158b
Ronnie Nigash <nigash.ronnie@gmail.com>
2014-02-07 12:31:41 -0800

    Changes up the Autonomous Command functionality

--- ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/commands/AutonomousCommand.java
+++ ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/commands/AutonomousCommand.java
@@ -25,10 +25,10 @@ public class  AutonomousCommand extends Command {
     }
     // Called just before this Command runs the first time
     protected void initialize() {
-        Robot.driveTrain.driveStraight(0.50);
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        Robot.driveTrain.driveStraight(0.40);
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
@@ -36,6 +36,7 @@ public class  AutonomousCommand extends Command {
     }
     // Called once after isFinished returns true
     protected void end() {
+        Robot.driveTrain.stop();
     }
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
