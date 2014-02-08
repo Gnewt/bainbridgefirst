62f657c9dbc7b2e64330be15944f4ad252aa5169
TarkanAl-Kazily <tarkucar@mac.com>
2014-02-08 15:23:19 -0800

    Returned to Keet's initial code.

--- ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/commands/Turn180.java
+++ ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/commands/Turn180.java
@@ -16,9 +16,9 @@ import org.usfirst.frc4915.ArcadeDriveRobot.Robot;
  */
 public class  Turn180 extends Command {
     
-    //private double degreesOfFreedom = 2.0;
+    private double degreesOfFreedom = 2.0;
     
-    private boolean shouldQuit;
+    //private boolean shouldQuit;
     
     public Turn180() {
         // Use requires() here to declare subsystem dependencies
@@ -30,7 +30,7 @@ public class  Turn180 extends Command {
     }
     // Called just before this Command runs the first time
     protected void initialize() {
-        /*
+        
         if (Robot.gyroscope == null) {
             end();
         }
@@ -39,31 +39,38 @@ public class  Turn180 extends Command {
         Robot.driveTrain.setMaxOutput(0.5);
         // set safety timeout
         setTimeout(6.0);
-        */
-        setTimeout(5.0);
         System.out.println("Began 180 turn");
+        /*
+        setTimeout(5.0);
+        
         shouldQuit = false;
         Robot.driveTrain.resetPIDValues();
+        */
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        System.out.println("Turning");
+        /*
+        
         shouldQuit = Robot.driveTrain.turnPID(180.0);
-        //Robot.driveTrain.turn();
+        */
+        System.out.println("Turning");
+        Robot.driveTrain.turn();
     }
     // Checks whether Robot has turned within a certain number of degrees from 180
     protected boolean isFinished() {
-        //return ((180.0-degreesOfFreedom < Robot.gyroscope.getAngle()) ||
-        //     (-180.0+degreesOfFreedom > Robot.gyroscope.getAngle()) || isTimedOut());
-        if (isTimedOut()) {
+        return ((170.0-degreesOfFreedom < Robot.gyroscope.getAngle()) ||
+             (-170.0+degreesOfFreedom > Robot.gyroscope.getAngle()) || isTimedOut());
+        /*if (isTimedOut()) {
             return true;
         }
         else {
             return shouldQuit;
         }
+        */
     }
     // Called once after isFinished returns true
     protected void end() {
+        Robot.gyroscope.reset();
         Robot.driveTrain.stop();
         System.out.println("Finished Turning");
     }
