b1053edf6ba2caf7061e281030eb5bd44d1dbacf
KeetCurtis <curtiskeet@gmail.com>
2014-02-09 15:25:33 -0800

    v1.08.10 Throttle values updated when stopped

--- ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/commands/ArcadeDrive.java
+++ ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/commands/ArcadeDrive.java
@@ -36,6 +36,7 @@ public class  ArcadeDrive extends Command {
         joystickDrive = Robot.oi.getJoystickDrive();
         joystickX = joystickDrive.getAxis(Joystick.AxisType.kX);
         joystickY = joystickDrive.getAxis(Joystick.AxisType.kY);
+        Robot.driveTrain.joystickThrottle = Robot.driveTrain.modifyThrottle();
         if ((Math.abs(joystickX) < 0.075) && (Math.abs(joystickY) < 0.075)) {
             Robot.driveTrain.stop();
         }
