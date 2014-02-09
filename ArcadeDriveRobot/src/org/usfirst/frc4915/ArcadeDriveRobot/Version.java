79aa9a6c7883193cf0fc6113f97d9cf40c8d354b
TarkanAl-Kazily <tarkucar@mac.com>
2014-02-08 16:35:40 -0800

    Version numbering changed

---
+++ ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/Version.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.usfirst.frc4915.ArcadeDriveRobot;
+
+/**
+ *
+ * @author Tarkan
+ */
+public class Version {
+    
+    private static final String VERSION = "v1.08.09";;
+    // Adds IntakeDown and IntakeUp commands
+    // Adds Magnetic Switch
+    // Changed buttons on Joystick to activate the Intake Down and Intake Up instead of Extend/Retract Pneumatics
+    // Fixed exceptions when there is no gyroscope (again)
+    // Added turnPID(double angle) which turns the robot using PID control (fixes)
+    // Testable turnPID(angle)
+    // Version system changed
+    // PID ratios altered
+    
+    public static String getVersion() {
+        return VERSION;
+    }
+}
