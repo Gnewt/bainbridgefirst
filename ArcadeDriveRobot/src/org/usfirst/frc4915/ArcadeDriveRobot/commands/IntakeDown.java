c88fcdefdf891cd76307541e76dbdacd0a8f5eea
TarkanAl-Kazily <tarkucar@mac.com>
2014-02-14 15:40:25 -0800

    Safeties added to WindLauncher command.

--- ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/commands/IntakeDown.java
+++ ArcadeDriveRobot/src/org/usfirst/frc4915/ArcadeDriveRobot/commands/IntakeDown.java
@@ -34,7 +34,7 @@ public class  IntakeDown extends Command {
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.harvester.getMagneticSwitchPneumaticsBottom();
+        return Robot.harvester.isLauncherDown();
     }
     // Called once after isFinished returns true
     protected void end() {
